965c08a3ccac9c03765d542ac9db01cacb628222
mikaek <mikaek@ntnu.stud.no>
2019-03-11 12:20:41 +0100

    Fixed package bug in Account

---
+++ src/com/teamfour/monopolish/Account/AccountDAO.java
@@ -0,0 +1,16 @@
+package com.teamfour.monopolish.Account;
+
+/**
+ * Handles database communication towards the 'Account' table in the database.
+ *
+ * @author      Eirik Hemstad
+ * @version     1.0
+ */
+
+public class AccountDAO {
+    // Attributes
+
+    public AccountDAO() {
+    }
+
+}
---
+++ src/com/teamfour/monopolish/Account/Tests/Account_test.java
@@ -0,0 +1,68 @@
+package com.teamfour.monopolish.Account.Tests;
+
+import com.teamfour.monopolish.accounts.Account;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+import java.time.LocalDate;
+import java.time.Month;
+
+public class Account_test {
+    private Account instance = null;
+
+    @BeforeAll
+    public void setInstance() {
+        LocalDate date = LocalDate.of(2019, Month.DECEMBER, 24);
+        instance = new Account("johhnyBoi23", "johhny@gmail.com", date, 10500);
+    }
+
+    @AfterAll
+    public void tearDownAll() {
+        instance = null;
+    }
+
+    @Test
+    //@DisplayName("Username")
+    public void getUsername() {
+        String result = instance.getUsername();
+        String expected = "johhnyBoi23";
+        assertEquals(result, expected);
+    }
+
+    @Test
+    @DisplayName("Email")
+    public void getEmail() {
+        String result = instance.getEmail();
+        String expected = "johhny@gmail.com";
+        assertEquals(result, expected);
+    }
+
+    @Test
+    @DisplayName("Date")
+    public void getDate() {
+        LocalDate result = instance.getRegDate();
+        LocalDate expected = LocalDate.of(2019, Month.DECEMBER, 24);
+        assertEquals(result, expected);
+    }
+
+    @Test
+    @DisplayName("Get Highscore")
+    public void getHighscore() {
+        int result = instance.getHighscore();
+        int expected = 10500;
+        assertEquals(result, expected);
+    }
+
+    @Test
+    @DisplayName("Set highscore")
+    public void setHighscore() {
+        instance.setHighscore(10000);
+        int result = instance.getHighscore();
+        int expected = 10000;
+        assertEquals(result, expected);
+    }
+}
