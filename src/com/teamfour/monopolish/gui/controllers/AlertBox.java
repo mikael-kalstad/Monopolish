f503600810058b8b66e0358e770bdb553eecf11f
baardshe <baardshe@stud.ntnu.no>
2019-03-11 15:13:13 +0100

    added AlertBox.java

---
+++ src/com/teamfour/monopolish/gui/controllers/AlertBox.java
@@ -0,0 +1,13 @@
+import javafx.scene.control.Alert;
+
+public class AlertBox {
+
+    public static void display(Alert.AlertType alertType, String title, String header, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(message);
+
+        alert.showAndWait();
+    }
+}
