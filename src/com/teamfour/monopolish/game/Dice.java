e35c5a886b1e6aab84afee7323ef7db17e1e96db
mikaek <mikaek@ntnu.stud.no>
2019-03-11 09:44:39 +0100

    Added dice class and Junit lib

--- src/com/teamfour/monopolish/game/Dice.java
+++ src/com/teamfour/monopolish/game/Dice.java
@@ -1,4 +1,37 @@
 package com.teamfour.monopolish.game;
 
+import java.util.Random;
+
 public class Dice {
+    private Random random = new Random();
+    private int numOfDices;
+    private int numOfEyes;
+
+    public Dice(int numOfDices, int numOfEyes) {
+        this.numOfDices = numOfDices;
+        this.numOfEyes = numOfEyes;
+    }
+
+    private int randomNum(int min, int max) {
+        return random.nextInt(max-min) + min;
+    }
+
+    public int[] throwDice() {
+        int[] result = new int[numOfDices];
+
+        for (int i = 0; i < numOfDices; i++) {
+            result[i] = randomNum(1, numOfEyes);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Dice dice = new Dice(2, 6);
+        int[] result = dice.throwDice();
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.println("Dice " + i + " " + result[i]);
+        }
+    }
 }
---
+++ src/com/teamfour/monopolish/game/Tests/Dice_test.java
@@ -0,0 +1,7 @@
+package com.teamfour.monopolish.game.Tests;
+
+import org.junit.jupiter.api.Test;
+
+public class Dice_test {
+
+}
