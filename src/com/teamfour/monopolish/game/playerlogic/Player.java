5dbb76ec68f320032a0c74f77eb2d9c711ee17d4
lisawil <lisawil@stud.ntnu.no>
2019-03-12 13:21:12 +0100

    small fixes propertyManager

--- src/com/teamfour/monopolish/game/playerlogic/Player.java
+++ src/com/teamfour/monopolish/game/playerlogic/Player.java
@@ -1,4 +1,40 @@
 package com.teamfour.monopolish.game.playerlogic;
 
 public class Player {
+    private final String username;
+    private int money = 8000;
+    private int position = 0;
+
+    public Player(String username){
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public void addMoney(int newMoney){
+        money += newMoney;
+    }
+
+    public void move(int steps){
+        position+=steps;
+    }
+
 }
