e89430d34ff89edf1d9dbf2b693a545c44f696e5
Lisa Willa <lisawil@stud.ntnu.no>
2019-03-12 09:28:17 +0100

    Upload New File

---
+++ src/com/teamfour/monopolish/game/propertylogic/Property.java
@@ -0,0 +1,78 @@
+package com.teamfour.monopolish.game.PropertyLogic;
+
+public class Property {
+    private int id;
+    private String name;
+    private int price;
+    private int position;
+    private boolean pawned = false;
+    private int owner = 0;
+    private String categorycolor;
+
+    public Property(int p_id, String name, int price, int position, String categorycolor){
+        this.id = p_id;
+        this.name = name;
+        this.price = price;
+        this.position = position;
+        this.categorycolor = categorycolor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public boolean isPawned() {
+        return pawned;
+    }
+
+    public int getOwner() {
+        return owner;
+    }
+
+    public void setPawned(boolean pawned) {
+        this.pawned = pawned;
+    }
+
+    public void setOwner(int owner) {
+        this.owner = owner;
+    }
+
+    public int compareTo(Property otherP){
+        if(otherP == null){
+            return(-2);
+        }
+        if(otherP.getPrice() > this.price){
+            return(-1);
+        }
+        if(otherP.getPrice()<this.price){
+            return(1);
+        }
+        else{
+            return(0);
+        }
+    }
+    public String getCategorycolor(){
+        return categorycolor;
+    }
+    public boolean equals(Property otherP){
+        if(otherP == null){
+            return(false);
+        }
+        if(this == otherP){
+            return (true);
+        }
+        return(otherP.getId() == this.id);
+    }
+}
