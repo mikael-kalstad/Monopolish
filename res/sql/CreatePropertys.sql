56c8f8107cb9fb485853b14ab6971fe0880c9725
tblauvvik <tb@lauvvik.no>
2019-03-13 12:29:25 +0100

    Commit

---
+++ res/sql/CreatePropertys.sql
@@ -0,0 +1,20 @@
+/**
+  Generates propertys
+ */
+
+INSERT INTO property VALUES(1, 'Parkveien', 1200, 'Pink');
+INSERT INTO property VALUES(2, 'Kirkeveien', 1200, 'Pink');
+
+INSERT INTO property VALUES(3, 'Kongens gate', 2000, 'Turquoise');
+INSERT INTO property VALUES(4, 'Prinsens gate', 2000, 'Turquoise');
+INSERT INTO property VALUES(5, 'Øvre slottsgate', 2400, 'Turquoise');
+
+INSERT INTO property VALUES(6, 'Nedre Slottsgate', 2800, 'Small');
+INSERT INTO property VALUES(7, 'Trondheimsveien', 2800, 'Small');
+INSERT INTO property VALUES(8, 'Nobels gate', 3200, 'Small');
+
+INSERT INTO property VALUES(1, 'Parkveien', 1200, 'Pink');
+INSERT INTO property VALUES(2, 'Kirkeveien', 1200, 'Pink');
+INSERT INTO property VALUES(3, 'Kongens gate', 2000, 'Turquoise');
+
+INSERT INTO property VALUES(4, 'Prinsens gate', 2000, 'Turquoise');
--- src/com/teamfour/monopolish/game/GameLogic.java
+++ src/com/teamfour/monopolish/game/GameLogic.java
@@ -1,18 +1,21 @@
 package com.teamfour.monopolish.game;
 
+import com.teamfour.monopolish.game.board.Board;
+import com.teamfour.monopolish.game.playerlogic.Player;
+
 import java.util.ArrayList;
 
 public class GameLogic {
-    //private GameDAO gameDAO;
+    private GameDAO gameDAO;
     private int gameId;
     private Dice dice = new Dice(2,6);
     private int turn;
     private int roundNumber;
     private boolean finished;
     private boolean yourTurn;
-    //private Board board;
+    private Board board;
 
-    public GameLogic(int gameId /* ArrayList<Player> players*/) {
+    public GameLogic(int gameId, ArrayList<Player> players) {
 
 
     }
