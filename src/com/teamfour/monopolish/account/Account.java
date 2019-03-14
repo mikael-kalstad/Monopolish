8cf38b66d10fbe9352c51fede8ac43a8decfbfda
mikaek <mikaek@ntnu.stud.no>
2019-03-14 13:33:37 +0100

    Revert "Commit"
    This reverts commit a955e819

--- src/com/teamfour/monopolish/account/Account.java
+++ src/com/teamfour/monopolish/account/Account.java
@@ -13,11 +13,13 @@ public class Account {
     private String username;
     private String email;
     private LocalDate regDate;
+    private int highscore;
 
-    public Account(String username, String email, LocalDate regDate) {
+    public Account(String username, String email, LocalDate regDate, int highscore) {
         this.username = username;
         this.email = email;
         this.regDate = regDate;
+        this.highscore = highscore;
     }
 
     public String getUsername() {
@@ -32,4 +34,11 @@ public class Account {
         return regDate;
     }
 
+    public int getHighscore() {
+        return highscore;
+    }
+
+    public void setHighscore(int newHighscore) {
+        this.highscore = newHighscore;
+    }
 }
