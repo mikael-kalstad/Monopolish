ea848abc2fe83c8b39574a36430dd59c28a79fca
lisawil <lisawil@stud.ntnu.no>
2019-03-18 14:54:08 +0100

    Create property

--- res/sql/CreatePropertys.sql
+++ res/sql/CreatePropertys.sql
@@ -13,21 +13,21 @@ INSERT INTO property VALUES(6, 'Nedre Slottsgate', 2800, 10, 'Pink');
 INSERT INTO property VALUES(7, 'Trondheimsveien', 2800, 12, 'Pink');
 INSERT INTO property VALUES(8, 'Nobels gate', 3200, 13, 'Pink');
 
-INSERT INTO property VALUES(9, 'Parkveien', 1200, 14, 'Orange');
-INSERT INTO property VALUES(10, 'Kirkeveien', 1200, 16, 'Orange');
-INSERT INTO property VALUES(11, 'Kongens gate', 2000, 17, 'Orange');
+INSERT INTO property VALUES(9, 'Grensen', 3600, 14, 'Orange');
+INSERT INTO property VALUES(10, 'Gabels gate', 3600, 16, 'Orange');
+INSERT INTO property VALUES(11, 'Ringgata', 4000, 17, 'Orange');
 
-INSERT INTO property VALUES(12, 'Prinsens gate', 2000, 19, 'Red');
-INSERT INTO property VALUES(13, 'Prinsens gate', 2000, 21, 'Red');
-INSERT INTO property VALUES(14, 'Prinsens gate', 2000, 22, 'Red');
+INSERT INTO property VALUES(12, 'Bygdøy Allé', 4400, 19, 'Red');
+INSERT INTO property VALUES(13, 'Skarpsno', 4400, 21, 'Red');
+INSERT INTO property VALUES(14, 'Slemdal', 4800, 22, 'Red');
 
-INSERT INTO property VALUES(15, 'Prinsens gate', 2000, 23, 'Yellow');
-INSERT INTO property VALUES(16, 'Prinsens gate', 2000, 24, 'Yellow');
-INSERT INTO property VALUES(17, 'Prinsens gate', 2000, 26, 'Yellow');
+INSERT INTO property VALUES(15, 'Karl Johans gate', 5200, 23, 'Yellow');
+INSERT INTO property VALUES(16, 'Stortorget', 5200, 24, 'Yellow');
+INSERT INTO property VALUES(17, 'Torggata', 5600, 26, 'Yellow');
 
-INSERT INTO property VALUES(18, 'Prinsens gate', 2000, 28, 'Green');
-INSERT INTO property VALUES(19, 'Prinsens gate', 2000, 29, 'Green');
-INSERT INTO property VALUES(20, 'Prinsens gate', 2000, 31, 'Green');
+INSERT INTO property VALUES(18, 'Trosterudveien', 6000, 28, 'Green');
+INSERT INTO property VALUES(19, 'Pilestredet', 6000, 29, 'Green');
+INSERT INTO property VALUES(20, 'Sinsen', 6400, 31, 'Green');
 
-INSERT INTO property VALUES(21, 'Prinsens gate', 2000, 34, 'Blue');
-INSERT INTO property VALUES(22, 'Prinsens gate', 2000, 36, 'Blue');
+INSERT INTO property VALUES(21, 'Ullevål Hageby', 7000, 34, 'Blue');
+INSERT INTO property VALUES(22, 'Rådusplassen', 8000, 36, 'Blue');
