e895ebb53c1bf2f15aa5803e2ed12c702f20143b
lisawil <lisawil@stud.ntnu.no>
2019-03-20 14:22:03 +0100

    try-catch LobbyDAO

--- src/com/teamfour/monopolish/lobby/LobbyDAO.java
+++ src/com/teamfour/monopolish/lobby/LobbyDAO.java
@@ -1,5 +1,6 @@
 package com.teamfour.monopolish.lobby;
 
+import com.mysql.cj.xdevapi.SqlDataResult;
 import com.teamfour.monopolish.database.DataAccessObject;
 
 import java.sql.ResultSet;
@@ -23,32 +24,45 @@ public class LobbyDAO extends DataAccessObject {
      * @return The lobby id
      * @throws SQLException
      */
-    public int insertLobby(String username) throws SQLException {
-        getConnection();
-        cStmt = connection.prepareCall("{call lobby_insert(?, ?)}");
-
-        cStmt.setString(1, username);
-        cStmt.registerOutParameter(2, Types.INTEGER);
-
+    public int insertLobby(String username) {
         int roomId = -1;
-        if (cStmt.executeUpdate() > 0)
-            roomId = cStmt.getInt(2);
+        try {
+            getConnection();
+            cStmt = connection.prepareCall("{call lobby_insert(?, ?)}");
+
+            cStmt.setString(1, username);
+            cStmt.registerOutParameter(2, Types.INTEGER);
+
+
+            if (cStmt.executeUpdate() > 0)
+                roomId = cStmt.getInt(2);
+        }catch (SQLException sql){
+            sql.printStackTrace();
+        }finally {
+            releaseConnection();
+        }
 
         return roomId;
     }
 
 
-    public int newLobby(String username, String lobbyname) throws SQLException {
-        getConnection();
-        cStmt = connection.prepareCall("{call new_lobby(?, ?, ?)}");
-
-        cStmt.setString(1, username);
-        cStmt.setString(2, lobbyname);
-        cStmt.registerOutParameter(3, Types.INTEGER);
-
+    public int newLobby(String username, String lobbyname) {
         int lobby_id = -1;
-        if (cStmt.executeUpdate() > 0) lobby_id = cStmt.getInt(3);
+        try {
+            getConnection();
+            cStmt = connection.prepareCall("{call new_lobby(?, ?, ?)}");
 
+            cStmt.setString(1, username);
+            cStmt.setString(2, lobbyname);
+            cStmt.registerOutParameter(3, Types.INTEGER);
+
+
+            if (cStmt.executeUpdate() > 0) lobby_id = cStmt.getInt(3);
+        }catch (SQLException sql){
+            sql.printStackTrace();
+        }finally{
+            releaseConnection();
+        }
         return lobby_id+1;
     }
 
@@ -59,17 +73,23 @@ public class LobbyDAO extends DataAccessObject {
      * @return True if successful
      * @throws SQLException
      */
-    public boolean addPlayer(String username, int lobby_id) throws SQLException {
-        getConnection();
-        cStmt = connection.prepareCall("{call join_lobby(?, ?, ?)}");
-
-        cStmt.setString(1, username);
-        cStmt.setInt(2, lobby_id);
-        cStmt.registerOutParameter(3, Types.BOOLEAN);
-
+    public boolean addPlayer(String username, int lobby_id) {
         boolean res = false;
-        if (cStmt.executeUpdate() > 0) res  = cStmt.getBoolean(3);
+        try {
+            getConnection();
+            cStmt = connection.prepareCall("{call join_lobby(?, ?, ?)}");
 
+            cStmt.setString(1, username);
+            cStmt.setInt(2, lobby_id);
+            cStmt.registerOutParameter(3, Types.BOOLEAN);
+
+
+            if (cStmt.executeUpdate() > 0) res = cStmt.getBoolean(3);
+        }catch (SQLException sql){
+            sql.printStackTrace();
+        }finally {
+            releaseConnection();
+        }
         return res;
     }
 
@@ -80,15 +100,21 @@ public class LobbyDAO extends DataAccessObject {
      * @return True if successful
      * @throws SQLException
      */
-    public boolean removePlayer(String username, int lobby_id) throws SQLException {
-        getConnection();
-        cStmt = connection.prepareCall("{call lobby_delete_user(?, ?)}");
+    public boolean removePlayer(String username, int lobby_id) {
+        int count = 0;
+        try {
+            getConnection();
+            cStmt = connection.prepareCall("{call lobby_delete_user(?, ?)}");
 
-        cStmt.setInt(1, lobby_id);
-        cStmt.setString(2, username);
+            cStmt.setInt(1, lobby_id);
+            cStmt.setString(2, username);
 
-        int count = cStmt.executeUpdate();
-
+            count = cStmt.executeUpdate();
+        }catch (SQLException sql){
+            sql.printStackTrace();
+        }finally {
+            releaseConnection();
+        }
         return (count > 0);
     }
 
@@ -100,17 +126,23 @@ public class LobbyDAO extends DataAccessObject {
      * @return True if successful
      * @throws SQLException
      */
-    public boolean setReady(int roomId, String username, boolean ready) throws SQLException {
-        getConnection();
-        System.out.println("Setting ready in procedure.. " + ready);
-        cStmt = connection.prepareCall("{call lobby_set_player_ready(?, ?, ?)}");
+    public boolean setReady(int roomId, String username, boolean ready) {
+        int count = 0;
+        try {
+            getConnection();
+            System.out.println("Setting ready in procedure.. " + ready);
+            cStmt = connection.prepareCall("{call lobby_set_player_ready(?, ?, ?)}");
 
-        cStmt.setInt(1, roomId);
-        cStmt.setString(2, username);
-        cStmt.setBoolean(3, ready);
+            cStmt.setInt(1, roomId);
+            cStmt.setString(2, username);
+            cStmt.setBoolean(3, ready);
 
-        int count = cStmt.executeUpdate();
-
+            count = cStmt.executeUpdate();
+        }catch (SQLException sql){
+            sql.printStackTrace();
+        }finally {
+            releaseConnection();
+        }
         return (count > 0);
     }
 
@@ -120,14 +152,20 @@ public class LobbyDAO extends DataAccessObject {
      * @return True if successful
      * @throws SQLException
      */
-    public boolean deleteLobby(int roomId) throws SQLException {
-        getConnection();
-        cStmt = connection.prepareCall("{call lobby_delete(?)}");
+    public boolean deleteLobby(int roomId) {
+        int count = 0;
+        try {
+            getConnection();
+            cStmt = connection.prepareCall("{call lobby_delete(?)}");
 
-        cStmt.setInt(1, roomId);
+            cStmt.setInt(1, roomId);
 
-        int count = cStmt.executeUpdate();
-
+            count = cStmt.executeUpdate();
+        }catch (SQLException sql){
+            sql.printStackTrace();
+        }finally {
+            releaseConnection();
+        }
         return (count > 0);
     }
 
@@ -138,38 +176,44 @@ public class LobbyDAO extends DataAccessObject {
      */
     public ArrayList<String> getUsersInLobby(int roomId) throws SQLException {
         ArrayList<String> users = new ArrayList<>();
+        try {
 
-        getConnection();
-        cStmt = connection.prepareCall("{CALL lobby_get_users_in_lobby(?)}");
+            getConnection();
+            cStmt = connection.prepareCall("{CALL lobby_get_users_in_lobby(?)}");
 
-        cStmt.setInt(1, roomId);
+            cStmt.setInt(1, roomId);
 
-        ResultSet rs = cStmt.executeQuery();
+            ResultSet rs = cStmt.executeQuery();
 
-        while (rs.next()) {
-            users.add(rs.getString(1));
+            while (rs.next()) {
+                users.add(rs.getString(1));
+            }
+        }catch (SQLException sql){
+            sql.printStackTrace();
+        }finally {
+            releaseConnection();
         }
-
-        releaseConnection();
-
         return users;
     }
 
     public ArrayList<String[]> getAllLobbies() throws SQLException {
         ArrayList<String[]> lobbyInfo = new ArrayList<>();
+        try {
 
-        getConnection();
-        cStmt = connection.prepareCall("{CALL getAllLobbies()}");
+            getConnection();
+            cStmt = connection.prepareCall("{CALL getAllLobbies()}");
 
-        ResultSet rs = cStmt.executeQuery();
+            ResultSet rs = cStmt.executeQuery();
 
-        while (rs.next()) {
-            String[] info = {rs.getString(1), rs.getString(2), String.valueOf(rs.getBoolean(3)), rs.getString(4)};
-            lobbyInfo.add(info);
+            while (rs.next()) {
+                String[] info = {rs.getString(1), rs.getString(2), String.valueOf(rs.getBoolean(3)), rs.getString(4)};
+                lobbyInfo.add(info);
+            }
+        }catch (SQLException sql){
+            sql.printStackTrace();
+        }finally {
+            releaseConnection();
         }
-
-        releaseConnection();
-
         return lobbyInfo;
     }
 
