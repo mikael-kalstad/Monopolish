f1f1eff2bcf24f0d625f4149a9ce412ee6a98d9b
Eirik Hemstad <eirikzh@gmail.com>
2019-03-21 11:40:41 +0100

    Hrmph

--- src/com/teamfour/monopolish/gui/controllers/GameController.java
+++ src/com/teamfour/monopolish/gui/controllers/GameController.java
@@ -95,7 +95,7 @@ public class GameController {
 
     public void yourTurn() {
         try {
-            gameLogic.newTurn();
+            gameLogic.startYourTurn();
         } catch (SQLException e) {
             e.printStackTrace();
         }
