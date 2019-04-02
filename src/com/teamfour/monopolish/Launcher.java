e53b2ddb21edff52018a3e7e34ab0c5e8ce42ee1
mikaek <mikaek@ntnu.stud.no>
2019-04-02 13:04:33 +0200

    Moved start method of app to a seperate launcher and refactored Handler. Also added username limitation (max 15 characters)

---
+++ src/com/teamfour/monopolish/Launcher.java
@@ -0,0 +1,57 @@
+package com.teamfour.monopolish;
+
+import com.teamfour.monopolish.gui.controllers.Handler;
+import com.teamfour.monopolish.gui.views.SceneManager;
+import com.teamfour.monopolish.gui.views.ViewConstants;
+import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.image.Image;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+    // Constants for Application
+    private final String APPLICATION_TITLE = "Monopoly";
+    private final String APPLICATION_LOGO = "res/gui/Dices/dice4.png";
+    private final double ASPECT_RATIO = 64.0/35.0;
+    private final String INITIAL_VIEW = ViewConstants.LOGIN.getValue();
+
+    /**
+     * This method will start the application on the initial view
+     *
+     * @param primaryStage The target window
+     */
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle(APPLICATION_TITLE);
+        primaryStage.getIcons().add(new Image("file:" + APPLICATION_LOGO));
+
+        // Setting full screen size to stage
+        Rectangle2D screen = Screen.getPrimary().getVisualBounds();
+        primaryStage.setWidth(screen.getWidth());
+        primaryStage.setHeight(screen.getHeight());
+        primaryStage.setMaximized(true);
+
+        // Setting initial view (login)
+        Handler.setSceneManager(new SceneManager(primaryStage, INITIAL_VIEW));
+        primaryStage.show();
+
+        // Event listener for resizing
+        ChangeListener<Number> stageSizeListener = (observable, oldValue, newValue) -> {
+            // Fixing scene scaling issues
+            Handler.getSceneManager().setSceneScale(primaryStage);
+
+            // Setting height
+            primaryStage.setHeight(primaryStage.getWidth() / (ASPECT_RATIO));
+        };
+
+        primaryStage.widthProperty().addListener(stageSizeListener);
+        primaryStage.heightProperty().addListener(stageSizeListener);
+
+//        System.out.println("visualbounds width: " + screen.getWidth()); // screens usable width (no task bars etc.)
+//        System.out.println("visualbounds height: " + screen.getHeight()); // screens usable height
+//        System.out.println("Actual res width: " + primaryStage.getWidth());
+//        System.out.println("Actual res height: " + primaryStage.getHeight());
+    }
+}
