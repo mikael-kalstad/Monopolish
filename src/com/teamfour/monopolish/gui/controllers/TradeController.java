660330560ea99adc1f3e800bfe3253edc4d75381
baardshe <baardshe@stud.ntnu.no>
2019-04-02 10:58:41 +0200

    tradescreen for recieving player

--- src/com/teamfour/monopolish/gui/controllers/TradeController.java
+++ src/com/teamfour/monopolish/gui/controllers/TradeController.java
@@ -17,6 +17,11 @@ public class TradeController {
     @FXML private Label tradeusername, yourtrademoney, requestedtrademoney, invalidinput, invalidinput2;
     @FXML private TextField offeredmoney, requestedmoney;
 
+    //on proposing trade to another player
+    @FXML private FlowPane playersOffer, askingFor;
+    @FXML private Label playerisproposing, PlayerIsOfferingLabel, offeredMoneyLabel, requestedMoneyLabel;
+    @FXML private Button acceptTradeBtn, refuseTradeBtn;
+
     // Username of the opponent you want to trade with
     private final String TRADE_USERNAME = Handler.getTradeUsername();
 
@@ -131,6 +136,8 @@ public class TradeController {
             ArrayList<String> offeredPropertiesNameList = new ArrayList<>();
             ArrayList<String> requestedPropertiesNameList = new ArrayList<>();
 
+            ArrayList<Object> finaloffer = new ArrayList<>();
+
             for (Pane p : offeredCards) {
                 offeredPropertiesNameList.add(p.getId());
             }
@@ -150,4 +157,41 @@ public class TradeController {
             if (container != null) container.setVisible(false);
         });
     }
+
+    public void showTrade(String playername, ArrayList<Property> offeredproperties, ArrayList<Property> requestedproperties,
+                          int offeredMoney, int requestedMoney) {
+
+        playerisproposing.setText(playername + " is proposing a trade");
+        PlayerIsOfferingLabel.setText(playername + " is offering:");
+
+        ArrayList<Pane> offeredCards = new ArrayList<>();
+        ArrayList<Pane> requestedCards = new ArrayList<>();
+
+        for (Property property : offeredproperties){
+            offeredCards.add(GameControllerDrawFx.createPropertyCard(property));
+        }
+
+        for (Property property : requestedproperties){
+            requestedCards.add(GameControllerDrawFx.createPropertyCard(property));
+        }
+
+        playersOffer.getChildren().addAll(offeredCards);
+        askingFor.getChildren().addAll(requestedCards);
+
+        offeredMoneyLabel.setText(Integer.toString(offeredMoney));
+        requestedMoneyLabel.setText(Integer.toString(requestedMoney));
+
+        acceptTradeBtn.setOnAction(e -> {
+            //do the transaction
+            //clear stuff
+            //send to eventlog "X accepted Y's trade"
+        });
+
+        refuseTradeBtn.setOnAction(e -> {
+            //close window
+            //clear stuff
+            //send to eventlog "X refused Y's trade"
+        });
+    }
+
 }
