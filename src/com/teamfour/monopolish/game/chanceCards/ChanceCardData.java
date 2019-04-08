ef575adba56f89a02872b190b5c8d297acf02a9e
mikaek <mikaek@ntnu.stud.no>
2019-04-08 15:00:09 +0200

    Uncommented lines in chanceCardData that was meant for testing purposes.

--- src/com/teamfour/monopolish/game/chanceCards/ChanceCardData.java
+++ src/com/teamfour/monopolish/game/chanceCards/ChanceCardData.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class ChanceCardData {
     private static ChanceCard[] data = new ChanceCard[]{
-            /*new ChanceCardBank("The bank does not like you, they demand $1200.", "file:res/gui/MessagePopup/dollarNegative.png", -1200),
+            new ChanceCardBank("The bank does not like you, they demand $1200.", "file:res/gui/MessagePopup/dollarNegative.png", -1200),
             new ChanceCardBank("You received $500 and roses from a secret lover.", "file:res/gui/ChanceCard/rose.png", 500),
             new ChanceCardBank("Studded tires are not allowed in central Trondheim, pay 500 in fines.", "file:res/gui/MessagePopup/dollarNegative.png", -500),
             new ChanceCardBank("On your way to work you stumbled upon a suit case with $3.000!", "file:res/gui/ChanceCard/suitcase.png", 3000),
@@ -14,10 +14,10 @@ public class ChanceCardData {
             new ChanceCardBank("Congratulations, you have won the lottery! Maybe it is time to buy that Yacht? Maybe not... 333.373 other people also won the lottery, total reward comes at $15.", "file:res/gui/MessagePopup/dollarPositive.png", 15),
             new ChanceCardBank("'Microsoft' called to warn you about some awful virus on your computer, you should have know something was up with that accent. Total cost $2500.", "file:res/gui/chanceCard/bug.png", -2500),
             new ChanceCardPosition("One of the most wonderful things in life is to wake up and enjoy a cuddle with somebody; unless you are in prison. To bad you are going to prison.", "file:res/gui/MessagePopup/handcuffs.png", 9),
-            new ChanceCardPosition("Go to start!", "file:res/gui/MessagePopup/again.png", 0),*/
+            new ChanceCardPosition("Go to start!", "file:res/gui/MessagePopup/again.png", 0),
             new ChanceCardPrank("You just got rick rolled, Rick Astley took all your money.", "file:res/gui/chanceCard/troll.png"),
-//            new ChanceCardPlayers("You did not like your opponents, so you stole $600 from each and everyone.", "file:res/gui/chanceCard/stealing.png", 600),
-//            new ChanceCardPlayers("You wrecked the cars to the other players, pay everyone $900 for mechanical repair fees.", "file:res/gui/chanceCard/crash.png", -900)
+            new ChanceCardPlayers("You did not like your opponents, so you stole $600 from each and everyone.", "file:res/gui/chanceCard/stealing.png", 600),
+            new ChanceCardPlayers("You wrecked the cars to the other players, pay everyone $900 for mechanical repair fees.", "file:res/gui/chanceCard/crash.png", -900)
     };
 
     public static ChanceCard getRandomChanceCard() {
