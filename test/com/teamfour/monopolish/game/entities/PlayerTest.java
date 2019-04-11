2c31d59d29a56fbe3126c1479d93698a045e4346
mikaek <mikaek@ntnu.stud.no>
2019-04-11 14:17:16 +0200

    Updated javadocs

--- test/com/teamfour/monopolish/game/entities/PlayerTest.java
+++ test/com/teamfour/monopolish/game/entities/PlayerTest.java
@@ -1,36 +1,39 @@
 package com.teamfour.monopolish.game.entities;
 
-import com.teamfour.monopolish.game.entities.Player;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 public class PlayerTest {
-
     private static Player instance = null;
 
     @BeforeAll
     public static void setInstance(){
-        instance = new Player("Grethe", 5000, 1, false, false, 1, false);
+        instance = new Player("Grethe", 5000, 0, false, false, 1, false);
+    }
+
+    @AfterAll
+    public static void tearDown(){
+        instance = null;
     }
 
     @Test
     @DisplayName("move test")
-    public void moveTest(){
+    public void moveTest() {
         instance.moveTo(0);
         instance.move(10);
-        assertTrue(instance.getPosition() == 10);
+        assertEquals(10, instance.getPosition());
     }
 
     @Test
     @DisplayName("moveTo test")
-    public void moveToTest(){
-        instance.moveTo(35);
-        assertTrue(instance.getPosition() == 35);
+    public void moveToTest() {
+        instance.move(0);
+        instance.moveTo(30);
+        assertEquals(30, instance.getPosition());
     }
 
     @Test
@@ -42,7 +45,7 @@ public class PlayerTest {
         }catch(IllegalArgumentException ie){
         }
 
-        assertTrue(instance.getPosition() == startPos);
+        assertEquals(instance.getPosition(), startPos);
     }
 
 }
